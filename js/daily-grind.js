2d5ac6fe57fb89de18834ea0d9573ff3cae719bd
TMerco <Tmercolino@sea.yearup.org>
2022-07-06 10:42:45 -0700

    adds wednesday to day of week alert

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,1 +1,24 @@
-alert()
+//alert("Is this working?")
+
+myDate = new Date ();
+myDay = myDate.getDay();
+
+switch(myDay){
+
+    case 0:
+        today = "Sunday";
+    break;
+
+    case 2:
+        today = "Tuesday";
+    break;
+
+    case 3:
+        today = "Wednesday";
+    break;
+
+    default:
+          today = "Something went wrong!";
+}
+
+alert(today)
