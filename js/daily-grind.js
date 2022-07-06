39557227aed09bc1be3c0e15616324d24c5cdb88
TMerco <Tmercolino@sea.yearup.org>
2022-07-06 11:28:40 -0700

    variables & Objects

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,7 +1,40 @@
 //alert("Is this working?")
 
-myDate = new Date ();
-myDay = myDate.getDay();
+
+/* 
+One unique image, with appropriate and matching content in the alt tag.  
+A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+A unique color that supports the image and paragraph of content
+
+
+pic - image src
+alt - the alt tag for the image
+desc - a description of the coffee
+day - the day of the week for the coffee
+color - color associated with coffee
+name - name of the coffee
+
+*/ 
+
+
+function coffeeTemplate(coffee){
+    let myReturn = "";
+
+    myReturn += `       
+        <p>
+            <img src="${coffee.pic}" alt="${coffee.alt}" />
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}
+        </p>
+    `;
+
+    return myReturn;
+
+}
+
+let myDate = new Date ();
+let myDay = myDate.getDay();
+let today = "";
+let coffee = "";
 
 switch(myDay){
 
@@ -15,10 +48,24 @@ switch(myDay){
 
     case 3:
         today = "Wednesday";
+
+        coffee = {
+            color: "Pink",
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            alt: "A picture of a bubble tea",
+            day: "Wednesday",
+            desc: `I like me some Bubble Tea!`
+        };
+
+
     break;
 
     default:
           today = "Something went wrong!";
 }
 
-alert(today)
+let coffeeData = coffeeTemplate(coffee)
+
+alert(coffeeData);
+console.log(coffee);
